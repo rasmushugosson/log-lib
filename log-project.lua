d43a4f7a7e441aa2ea1aa6ea97bda8f33706923e
Rasmus Hugosson <rasmus.hugosson01@gmail.com>
2026-01-19 02:18:47 +0100

    Fix duplicate submodules problem

--- log-project.lua
+++ log-project.lua
@@ -1,8 +1,17 @@
 -- Reusable Log project definition
 -- Can be included by parent projects via: include("path/to/log-lib/log-project.lua")
 
+-- Guard to prevent multiple inclusions
+if LOG_LIB_INCLUDED then
+    return
+end
+LOG_LIB_INCLUDED = true
+
 local log_lib_dir = path.getdirectory(_SCRIPT)
 local log_lib_src = log_lib_dir .. "/log-lib"
+
+-- Export include path for dependent projects
+LOG_LIB_INCLUDE_DIR = log_lib_src .. "/include"
 
 project("Log")
     kind("StaticLib")
